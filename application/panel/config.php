ff40411f6babb127af0c2307bb47ea703d7c5c7b
XZFFF <zefeng.xie@wutnews.net>
2018-08-05 02:25:03 +0800

    【chore】上传index主页和panel基本结构

---
+++ application/panel/config.php
@@ -0,0 +1,15 @@
+<?php
+//配置文件
+return [
+    //模板参数替换
+    'view_replace_str'       => array(
+        '__PANEL__'=>'/ziqiangweb_v3.0/public/index.php/panel',
+        '__CSS__'=>'/ziqiangweb_v3.0/public/static/css',
+        '__FONT__'=>'/ziqiangweb_v3.0/public/static/fonts',
+        '__JS__'=>'/ziqiangweb_v3.0/public/static/js',
+        '__IMG__' => '/ziqiangweb_v3.0/public/static/images',
+        '__UE__'=>'/ziqiangweb_v3.0/public/static/ueditor',
+        '__UM__'=>'/ziqiangweb_v3.0/public/static/umeditor',
+        '__DT__'=>'/ziqiangweb_v3.0/public/static/datatable',
+    ),
+];
