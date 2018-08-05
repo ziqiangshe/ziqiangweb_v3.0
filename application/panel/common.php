134cae06c43cd7e7eb56722e8364f6c841e0cf7a
XZFFF <zefeng.xie@wutnews.net>
2018-08-05 15:36:53 +0800

    【feat】config加盐、common规定API返回格式函数

--- application/panel/common.php
+++ application/panel/common.php
@@ -1,1 +1,17 @@
 <?php
+/**
+ * api返回格式化函数
+ * @param $errcode
+ * @param $errmsg
+ * @param $data
+ * @param $status
+ * @return \think\response\Json
+ */
+function apireturn($errcode, $errmsg, $data)
+{
+    return json([
+        'errcode' => $errcode,
+        'errmsg' => $errmsg,
+        'data' => $data
+    ]);
+}
